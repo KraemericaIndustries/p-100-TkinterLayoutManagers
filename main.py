0876ff540a3d6e7d395806b2ebb693bd0fc68923
KraemericaIndustries <bob.kraemer.git@gmail.com>
2024-09-18 13:33:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# Layout Managers:
+# Pack() - defaults to centre align, linear-layout(vertical)
+#     <object_name>.pack(side="left") to left align (top bottom, right also available)
+# Place() - x,y coordinates to explicitly position widgets
+# grid() - explicitly specify row and column keyword arguments to position widgets relative to each other
+
+from tkinter import *
+
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=200)
+
+#Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
+
+#Button
+button = Button(text="Click Me", command=button_clicked)
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
+
+#Entry
+input = Entry(width=10)
+print(input.get())
+input.grid(column=3, row=2)
+
+
+
+
+
+
+
+
+
+window.mainloop()
